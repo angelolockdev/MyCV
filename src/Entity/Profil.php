f55356d032f41163f40b57d5cc7d844b0d902a28
Angelo <locktohang@gmail.com>
2019-04-26 17:58:37 +0300

    correct commit with Mysql database
    Connection with Postgres heroku database is failed .

--- src/Entity/Profil.php
+++ src/Entity/Profil.php
@@ -2,7 +2,9 @@
 
 namespace App\Entity;
 
+use App\Utils\UtilsCV;
 use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\Integer;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProfilRepository")
@@ -22,9 +24,69 @@ class Profil
     private $first_name;
 
     /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateNaissance;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lieuNaissance;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="string", length=15, nullable=true)
+     */
+    private $telephone;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $biographie;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
      * @ORM\Column(type="string", length=255)
      */
     private $second_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $section_description;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $bio2;
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTime();
+    }
 
     public function getId(): ?int
     {
@@ -43,6 +105,114 @@ class Profil
         return $this;
     }
 
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->dateNaissance;
+    }
+
+    public function setDateNaissance(\DateTimeInterface $dateNaissance): self
+    {
+        $this->dateNaissance = $dateNaissance;
+
+        return $this;
+    }
+
+    public function getLieuNaissance(): ?string
+    {
+        return $this->lieuNaissance;
+    }
+
+    public function setLieuNaissance(string $lieuNaissance): self
+    {
+        $this->lieuNaissance = $lieuNaissance;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getBiographie(): ?string
+    {
+        return $this->biographie;
+    }
+
+    public function setBiographie(string $biographie): self
+    {
+        $this->biographie = $biographie;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
     public function getSecondName(): ?string
     {
         return $this->second_name;
@@ -53,5 +223,49 @@ class Profil
         $this->second_name = $second_name;
 
         return $this;
+    }
+    public function getFullName(){
+        return $this->getFirstName().' '.$this->getSecondName();
+    }
+
+    public function getAge(): ?string{
+        $naissanceYear = $this->getYear($this->getDateNaissance());
+        $nowYear = $this->getYear(new \DateTime());
+        $retour = $naissanceYear<$nowYear ? ($nowYear-$naissanceYear).' ans' : null;
+        return $retour;
+    }
+    public function getYear(?\DateTimeInterface $date): int
+    {
+        $age = date_parse($date->format('Y-m-d'));
+        return (int) "".$age['year'];
+    }
+
+    public function getSectionDescription(): ?string
+    {
+        return $this->section_description;
+    }
+
+    public function setSectionDescription(string $section_description): self
+    {
+        $this->section_description = $section_description;
+
+        return $this;
+    }
+
+    public function getBio2(): ?string
+    {
+        return $this->bio2;
+    }
+
+    public function setBio2(string $bio2): self
+    {
+        $this->bio2 = $bio2;
+
+        return $this;
+    }
+    public function experiences(){
+        $nowYear = $this->getYear(new \DateTime());
+        $ret = $nowYear - 2018;
+        return $ret;
     }
 }
